37b158c9abe289750e014ba993eb257fb678c94f
AungPhyoHein89 <aungsihein2014@gmail.com>
2026-05-28 16:20:57 +0630

    Using inner wrapper function

--- 01_decorator.py
+++ 01_decorator.py
@@ -1,8 +1,30 @@
-def add_spinkles(func):
-    print("**You add spinkles**")
-    func()
+# Without Using inner wrapper function
+# def add_spinkles(func):
+#     print("**You add spinkles**")
+#     func()
   
 
+# @add_spinkles
+# def ice_cream():
+#   print("Here is your ice cream")
+
+# Using Wrapper Function
+
+def add_spinkles(func):
+  def wrapper():
+    print("**You add spinkles**")
+    func()
+  return wrapper
+
+def add_fudge(func):
+  def wrapper():
+    print("**You add fudge**")
+    func()
+  return wrapper
+  
 @add_spinkles
-def ice_cream():
-  print("Here is your ice cream")
+@add_fudge
+def get_ice_cream():
+  print("Here is your ice cream.")
+
+get_ice_cream()
