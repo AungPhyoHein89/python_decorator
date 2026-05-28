c6b2355b8e4b848e381ff9395debae723690a662
AungPhyoHein89 <aungsihein2014@gmail.com>
2026-05-28 16:32:31 +0630

    Apply argument

--- 01_decorator.py
+++ 01_decorator.py
@@ -10,21 +10,42 @@
 
 # Using Wrapper Function
 
+# def add_spinkles(func):
+#   def wrapper():
+#     print("**You add spinkles**")
+#     func()
+#   return wrapper
+
+# def add_fudge(func):
+#   def wrapper():
+#     print("**You add fudge**")
+#     func()
+#   return wrapper
+  
+# @add_spinkles
+# @add_fudge
+# def get_ice_cream():
+#   print("Here is your ice cream.")
+
+# get_ice_cream()
+
+# Apply Argument
+
 def add_spinkles(func):
-  def wrapper():
+  def wrapper(*arg, **kwarg):
     print("**You add spinkles**")
-    func()
+    func(*arg, **kwarg)
   return wrapper
 
 def add_fudge(func):
-  def wrapper():
+  def wrapper(*arg, **kwarg):
     print("**You add fudge**")
-    func()
+    func(*arg, **kwarg)
   return wrapper
-  
+
 @add_spinkles
 @add_fudge
-def get_ice_cream():
-  print("Here is your ice cream.")
+def get_ice_cream(flavor):
+  print(f"Here is your {flavor} flavor ice cream.")
 
-get_ice_cream()
+get_ice_cream("chocolate")
